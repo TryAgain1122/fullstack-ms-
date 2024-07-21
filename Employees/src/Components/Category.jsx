7268f7320c2bc3471687b2a3a83cde023063dfbc
TryAgain1122 <106519847+TryAgain1122@users.noreply.github.com>
2024-07-21 23:16:06 +0800

    Update Category.jsx

--- Employees/src/Components/Category.jsx
+++ Employees/src/Components/Category.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 const Category = () => {
   const [category, setCategory] = useState([])
   useEffect(() => {
-    axios.get('http://localhost:3000/auth/category')
+    axios.get('https://employee-ms-seven.vercel.app/auth/category')
     .then(result => {
       if (result.data.Status) {
         setCategory(result.data.Result)
