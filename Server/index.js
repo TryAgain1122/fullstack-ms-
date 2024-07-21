07f6a53e7659b628b5b9320183e8347ac361d77a
TryAgain1122 <106519847+TryAgain1122@users.noreply.github.com>
2024-07-21 23:13:39 +0800

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://employee-backend-seven.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))
