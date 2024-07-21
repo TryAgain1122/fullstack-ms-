5ebdc1b007952c9897b4e90256a881e12bc0ea00
TryAgain1122 <106519847+TryAgain1122@users.noreply.github.com>
2024-07-21 23:46:47 +0800

    Update Login.jsx

--- Employees/src/Components/Login.jsx
+++ Employees/src/Components/Login.jsx
@@ -12,7 +12,7 @@ export const Login = () => {
     axios.defaults.withCredentials = true;
     const handleSubmit = (event) => {
         event.preventDefault()
-        axios.post('https://employee-ms-seven.vercel.app/adminlogin', values)
+        axios.post('https://employee-backend-seven.vercel.app/adminlogin', values)
             .then(result => {
                 if (result.data.loginStatus) {
                     localStorage.setItem("valid", true)
